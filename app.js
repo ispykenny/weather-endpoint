22b43342c7e7033ae9ea6f7016fd0b4330c727d2
ispykenny <kenny.krosky@gmail.com>
2020-09-29 06:45:31 -0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,20 @@
+require('dotenv').config();
+const express = require('express');
+const axios = require('axios')
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.get('/favicon.ico', (req,res)=>{
+  return 'your faveicon'
+ })
+
+app.get('/:forecast', (req, res) => {
+  let location = req.params.forecast;
+  axios.get(`https://api.openweathermap.org/data/2.5/forecast/daily?q=${location}&appid=${process.env.APIKEY}`)
+  .then((resp) => res.json(resp.data))
+  .catch((error) => res.json(error))
+})
+
+app.listen(PORT, () => {
+  console.log(`listening on port ${PORT}`)
+})
