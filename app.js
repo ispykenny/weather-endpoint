bad0419b5f01c4f2ad6dd35a3b7f0542dca19d44
ispykenny <kenny.krosky@gmail.com>
2020-09-29 06:51:28 -0700

    added access control

--- app.js
+++ app.js
@@ -4,6 +4,12 @@ const axios = require('axios')
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.get('/favicon.ico', (req,res)=>{
   return 'your faveicon'
  })
